90b7ce287c4be8314c74a14dc94fe8606bf155a4
imcvampire <quocanh96@gmail.com>
2016-12-17 22:19:38 +0700

    feat: login form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React from 'react';
 
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Login from './components/Login'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      isAuthenticated: false,
+      userData: {},
+    }
+  }
+
+  updateAuthen(isAuthenticated, userData) {
+    this.setState({
+      isAuthenticated,
+      userData,
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <main>
+          { this.state.isAuthenticated ? 'dfgdfg' : <Login update={this.updateAuthen.bind(this)}></Login> }
         </main>
         <Footer />
       </div>
